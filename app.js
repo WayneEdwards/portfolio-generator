e9be49351ecdad5fcc86f6743b14077e216b8844
Wayne Edwards <waedwards42@yahoo.com>
2021-04-08 14:53:59 -0700

    added VALIDATION property

--- app.js
+++ app.js
@@ -6,12 +6,28 @@ const promptUser = () => {
         .prompt([{
                 type: 'input',
                 name: 'name',
-                message: 'What is your name?'
+                message: 'What is your name? (Required)',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your name!')
+                    }
+
+                }
             },
             {
                 type: 'input',
                 name: 'github',
-                message: 'Enter you Github Username'
+                message: 'Enter you Github Username',
+                validate: nameInput => {
+                    if (nameInput) {
+                        return true;
+                    } else {
+                        console.log('Please enter your name!')
+                    }
+
+                }
             },
             {
                 type: 'input',
@@ -38,12 +54,28 @@ const promptProject = portfolioData => {
     return inquirer.prompt([{
             type: 'input',
             name: 'name',
-            message: 'What is the name of the project?'
+            message: 'What is the name of the project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!')
+                }
+
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!')
+                }
+
+            }
         },
         {
             type: 'checkbox',
@@ -54,7 +86,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your name!')
+                }
+
+            }
         },
         {
             type: 'confirm',
