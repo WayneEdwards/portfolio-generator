4d5230c4120b1079751fcc75d5211d37b9c6a9a5
Wayne Edwards <waedwards42@yahoo.com>
2021-04-08 14:01:13 -0700

    Installed npm inquirer

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2);
+// const fs = require('fs');
 
-const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js');
+
+// const pageHTML = generatePage(name, github);
+
+// const [name, github] = profileDataArgs;
 
 
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//    if (err) throw err;
 
-
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Check out index.html to see the output!')
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!')
+// });
